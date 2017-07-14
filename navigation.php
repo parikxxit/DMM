baca1fffb406b742d404e40f249fdea688d9bbb9
root <root@singhssolution.com>
2017-07-15 00:02:56 +0530

    Common Navigation Added

---
+++ navigation.php
@@ -0,0 +1,101 @@
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+			<div class="container-fluid">
+				<div class="navbar-header">
+				<!-- Mobile Toggle Menu Button -->
+				<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle visible-xs-block" data-toggle="collapse" data-target="#fh5co-navbar" aria-expanded="false" aria-controls="navbar"><i></i></a>
+				<a class="navbar-brand" href="/">Innovation</a>
+				</div>
+				<div id="fh5co-navbar" class="navbar-collapse collapse">
+					<ul class="nav navbar-nav navbar-right">
+					<!--	<li class="active"><a href="right-sidebar.html"><span> <span class="border"></span></span></a></li>-->
+	                    <li><a href="/DMM/about-us.php"><span>About Us<span class="border"></span></span></a></li>
+						<!--<li><a href="elements.html"><span>Elements <span class="border"></span></span></a></li>-->
+						<li><a href="/DMM/our-team.php"><span >Our Team<span class="border"></span></span></a></li>
+					</ul>
+	 <ul class="nav navbar-nav">
+      <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Canteens <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="/DMM/building/canteens/manit.php">Manit Canteen</a></li>
+            <li><a href="/DMM/building/canteens/dastoor.php">Dastoor Canteen</a></li>
+            <li><a href="/DMM/building/canteens/susangat.php">Susangat Canteen</a></li>
+            <li><a href="/DMM/building/canteens/amul-parlour">Amul Parlour</a></li>
+            <li><a href="/DMM/building/canteens/high-octane-cafe.php">High Octane Cafe</a></li>
+          </ul>
+        </li>
+     </ul>
+     <ul class="nav navbar-nav">
+      <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Hostels <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="/DMM/building/hostels/hostel_1.php">Hostel #1</a></li>
+            <li><a href="/DMM/building/hostels/hostel_2.php">Hostel #2</a></li>
+            <li><a href="/DMM/building/hostels/hostel_3.php">Hostel #3</a></li>
+            <li><a href="/DMM/building/hostels/hostel_4.php">Hostel #4</a></li>
+            <li><a href="/DMM/building/hostels/hostel_5.php">Hostel #5</a></li>
+            <li><a href="/DMM/building/hostels/hostel_6.php">Hostel #6</a></li>
+            <li><a href="/DMM/building/hostels/hostel_7.php">Hostel #7(Girl's Hostel)</a></li>
+            <li><a href="/DMM/building/hostels/hostel_8.php">Hostel #8</a></li>
+            <li><a href="/DMM/building/hostels/hostel_9.php">Hostel #9(Nri's hostel)</a></li>
+            <li><a href="/DMM/building/hostels/hostel_10.php">Hostel #10</a></li>
+          </ul>
+        </li>
+     </ul>
+     <ul class="nav navbar-nav">
+      <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Other Building <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="/DMM/building/other-building/rolta-club.php">Rolta Club</a></li>
+            <li><a href="/DMM/building/other-building/deans-office.php">Dean(s) Office</a></li>
+            <li><a href="/DMM/building/other-building/manit-school.php">Manit School</a></li>
+            <li><a href="/DMM/building/other-building/ntb.php">New Teaching Block/NTB</a></li>
+            <li><a href="/DMM/building/other-building/sac.php">Student Activity Center/SAC</a></li>
+            <li><a href="/DMM/building/other-building/admin-block.php">Admin Block</a></li>
+            <li><a href="/DMM/building/other-building/post-office.php">Post Office</a></li>
+            <li><a href="/DMM/building/other-building/sbi-atm.php">SBI ATM</a></li>
+            <li><a href="/DMM/building/other-building/dispensary.php">Dispensary</a></li>
+            <li><a href="/DMM/building/other-building/energy-center.php">Energy Center</a></li>
+            <li><a href="/DMM/building/other-building/manit-gate.php">MANIT Gate</a></li>
+            <li><a href="/DMM/building/other-building/power-station.php">Power Station</a></li>
+            <li><a href="/DMM/building/other-building/science-block.php">Science Block</a></li>
+          </ul>
+        </li>
+     </ul>
+     <ul class="nav navbar-nav">
+      <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Layouts <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="layouts/ntb1.jpg" download>Ntb Ground Floor</a></li>
+            <li><a href="layouts/ntb.jpg" download>Ntb First Floor</a></li>
+          </ul>
+        </li>
+     </ul>
+     <ul class="nav navbar-nav">
+      <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Departments <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="/DMM/building/departments/civil.php">Civil Deptt.</a></li>
+            <li><a href="/DMM/building/departments/mech.php">Mechanical Deptt.</a></li>
+            <li><a href="/DMM/building/departments/electrical.php">Electrical Deptt.</a></li>
+            <li><a href="/DMM/building/departments/cse.php">Computer Science Deptt.</a></li>
+            <li><a href="/DMM/building/departments/chemical.php">Chemical and MSME Deptt.</a></li>
+            <li><a href="/DMM/building/departments/ece.php">Electronics and Comm. Deptt.</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="/DMM/building/departments/maths.php">Mathematics Deptt.</a></li>
+            <li><a href="/DMM/building/departments/physics.php">Physics Deptt.</a></li>
+            <li><a href="/DMM/building/departments/chemistry.php">Chemistry Deptt.</a></li>
+          </ul>
+        </li>
+     </ul>
+     <ul class="nav navbar-nav">
+      <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Complexes<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="/DMM/building/complexes/sports-complex.php">Sports Complex</a></li>
+            <li><a href="/DMM/building/complexes/shopping-complex.php">Shopping Complex</a></li>
+          </ul>
+        </li>
+     </ul>
+				</div>
+			</div>
+		</nav>
